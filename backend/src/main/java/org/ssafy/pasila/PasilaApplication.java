f911167cd1902d32d697e0d2856aced896fd15e1
soyeon <jjellyduck@gmail.com>
2024-01-15 13:39:48 +0900

    feat: [#S10P12A402-23] init backend project

---
+++ backend/src/main/java/org/ssafy/pasila/PasilaApplication.java
@@ -0,0 +1,13 @@
+package org.ssafy.pasila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasilaApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/org/ssafy/pasila/domain/product/dto/LargeCategory.java
@@ -0,0 +1,7 @@
+package org.ssafy.pasila.domain.product.dto;
+
+public class LargeCategory {
+    private int id;
+    private String name;
+    private String imageUrl;
+}
