07ec4586d163997167eadf6e4fd05b486065f6f3
youkyoungJung <76727102+youkyoungJung@users.noreply.github.com>
2024-01-16 10:28:41 +0900

    feat: [#S10P12A402-8] fine-tuning

---
+++ backend/src/main/java/org/ssafy/pasila/global/model/ChatResponse.java
@@ -0,0 +1,33 @@
+package org.ssafy.pasila.global.model;
+
+import java.util.List;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class ChatResponse {
+
+    //    @JsonProperty("choices")
+    private List<Choice> choices;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Choice {
+        //        @JsonProperty("finish_reason")
+        private String finishReason;
+
+        //        @JsonProperty("index")
+        private int index;
+
+        //        @JsonProperty("message")
+        private Message message;
+
+    }
+
+}
---
+++ backend/src/main/java/org/ssafy/pasila/global/model/Message.java
@@ -0,0 +1,21 @@
+package org.ssafy.pasila.global.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Message {
+
+    private String content;
+
+    private String role;
+
+    public Message(String role, String prompt) {
+        this.role = role;
+        this.content = prompt;
+    }
+
+}
