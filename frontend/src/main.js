a4e83ac5cf1ceb37f157f5204cd101f8fbb10c40
최지우 <qlenfrl101@naver.com>
2024-01-16 17:46:07 +0900

    chore: [#S10P12A402-32] fontawesome 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,19 @@
-// import './assets/scss/_common.scss'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faCalendarCheck, faUser } from '@fortawesome/free-regular-svg-icons'
 
 import App from './App.vue'
 import router from './router'
 
+library.add(faMagnifyingGlass)
+library.add(faCalendarCheck, faUser)
+
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
