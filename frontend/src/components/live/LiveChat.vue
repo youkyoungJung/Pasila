28138882a442274959b92f353552d6e925193a93
최지우 <qlenfrl101@naver.com>
2024-01-17 17:38:43 +0900

    feat: [#S10P12A402-26] 웹캠 연결 및 라이브 방송 진행 화면 그리는중

---
+++ frontend/src/components/live/LiveChat.vue
@@ -0,0 +1,14 @@
+<script setup>
+const ChatList = [{ user: '김빨강', content: '그래서 기능이 먼데;' }]
+</script>
+
+<template>
+  <div class="live-chat"></div>
+</template>
+
+<style lang="scss" scoped>
+.live-chat {
+  @include box(100%, 100%, none, 20px, 1rem 0 1rem 0, 0.5rem 1rem);
+  border: 3px solid $main;
+}
+</style>
