3ebb104fc6c022d7851d4e3cf5d913d193c3d146
soyeon <jjellyduck@gmail.com>
2024-01-17 17:21:32 +0900

    feat: [#S10P12A402-94] 대본 생성 api 추가

--- backend/src/main/java/org/ssafy/pasila/global/infra/gpt3/controller/ChatController.java
+++ backend/src/main/java/org/ssafy/pasila/global/infra/gpt3/controller/ChatController.java
@@ -1,6 +1,8 @@
 package org.ssafy.pasila.global.infra.gpt3.controller;
 
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,46 +10,52 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.ssafy.pasila.global.infra.gpt3.GptClient;
 import org.ssafy.pasila.global.infra.gpt3.model.ChatRequest;
 import org.ssafy.pasila.global.infra.gpt3.model.ChatResponse;
 
 @Slf4j
+@AllArgsConstructor
 @RestController
 @RequestMapping("/gpt3")
 public class ChatController {
 
-    @Qualifier("openaiRestTemplate")
-    @Autowired
-    private RestTemplate restTemplate;
-
-    @Value("${openai.api.id}")
-    private String model;
-
-    @Value("${openai.api.url}")
-    private String apiUrl;
+    private final GptClient gptService;
 
     @GetMapping("/chat")
-    public String chat(@RequestParam String prompt) {
-        String system = "파챗은 급식체라고 불리며 초등학생, 유치한 사람입니다.";
-        // String system = "파챗은 나선욱체라고 불리며 허세넘치는 사람입니다.";
-
-        ChatRequest request = new ChatRequest(model, system, prompt);
-
-        // call the API
+    public String chat() {
         try {
-            ChatResponse response = restTemplate.postForObject(apiUrl, request, ChatResponse.class);
-            assert response != null;
+           String qsheet = gptService.generateQsheet(
+                   "올라프",
+                   " 컬러마스크 새부리형 30매 스타일핏 클래식",
+                   "직원들도 처음 보는 가격이라구요 !? \uD83D\uDE3F\n" +
+                           "어디서도 볼 수 없는 가격 -!\n" +
+                           "푸름웰니스 본사 방송 단 1시간 동안만 보여드립니다✨\n" +
+                           "\n" +
+                           "✅ KF94 클래식 SS 20매 : 3,180원 (장당 159원)\n" +
+                           "✅ 스타일핏 클래식 FW 30매 : 3,300원 (장당 110원)\n" +
+                           "\n" +
+                           "품절 시 재입고 불가!\n" +
+                           "남은 수량 내에서 꼭 GET하세요 \uD83D\uDC95" +
+                   "\uD83D\uDCA5품절대란 판매 1위 \uD83D\uDCA5 비움 디럭스\uD83D\uDCA5\n" +
+                           "\n" +
+                           "\uD83E\uDEE2마스크 맛집\uD83E\uDEE2 비움 디럭스 KF94 ❤\uFE0F\u200D\uD83D\uDD25\n" +
+                           "\n" +
+                           "화이트\uD83E\uDD0D그레이\uD83D\uDDA4 블랙 \uD83D\uDDA4\n" +
+                           "핑크베이지\uD83D\uDC97 라이트 실버\uD83E\uDD0D 아이보리\uD83D\uDC9B\n" +
+                           "\n" +
+                           "\uD83D\uDC4925매 - 6,450원 (장당 258원)\n" +
+                           "\n" +
+                           "\uD83D\uDCE64세트 이상 구매 시 무료배송\uD83D\uDCE6"
+           );
+           String qsheetStyle = gptService.chatStyle("초딩", qsheet);
 
-            if (response.getChoices() == null || response.getChoices().isEmpty()) {
-                return "No response";
-            }
-
-            // return the first response
-            return response.getChoices().get(0).getMessage().getContent();
+           return qsheetStyle;
 
         } catch (RestClientException e) {
             log.error("Error occurred while making the API request: {}", e.getMessage());
+            return e.getMessage();
         }
-        return prompt;
     }
+
 }
