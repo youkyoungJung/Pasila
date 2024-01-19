718453ea4c085073e056f917305445c9a2f95f57
최지우 <qlenfrl101@naver.com>
2024-01-19 17:54:11 +0900

    feat: [#S10P12A402-28] 캘린더, 프로덕트 카드 화면 진행중

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LiveView from '../views/LiveView.vue'
+import ScheduleView from '../views/ScheduleView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/live/:id',
       name: 'live',
       component: LiveView
+    },
+    {
+      path: '/schedule',
+      name: 'schedule',
+      component: ScheduleView
     }
   ]
 })
