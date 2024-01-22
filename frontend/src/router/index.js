693d72e70117b5539bd542bf469e8cb820811fd4
최지우 <qlenfrl101@naver.com>
2024-01-22 15:35:25 +0900

    feat: [#S10P12A402-29] ChannelView view, router 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,12 @@
-
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-import LoginView from '../views/LoginView.vue';
-import JoinView from '../views/JoinView.vue';
-import FindPwView from '../views/FindPwView.vue';
-import FindPwSendView from '@/views/FindPwSendView.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import JoinView from '../views/JoinView.vue'
+import FindPwView from '../views/FindPwView.vue'
+import FindPwSendView from '@/views/FindPwSendView.vue'
 import LiveView from '../views/LiveView.vue'
 import ScheduleView from '../views/ScheduleView.vue'
+import ChannelView from '../views/ChannelView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,18 +24,19 @@ const router = createRouter({
     {
       path: '/join',
       name: 'join',
-      component: JoinView,
+      component: JoinView
     },
     {
       path: '/findpw',
       name: 'findpw',
-      component: FindPwView,
+      component: FindPwView
     },
     {
       path: '/findpw/send',
       name: 'findpwsend',
-      component: FindPwSendView,
-    },{
+      component: FindPwSendView
+    },
+    {
       path: '/live/:id',
       name: 'live',
       component: LiveView
@@ -44,6 +45,11 @@ const router = createRouter({
       path: '/schedule',
       name: 'schedule',
       component: ScheduleView
+    },
+    {
+      path: '/channel/:id',
+      name: 'channel',
+      component: ChannelView
     }
   ]
 })
---
+++ frontend/src/views/ChannelView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>내 채널입니다.</div>
+</template>
+
+<style lang="scss" scoped></style>
