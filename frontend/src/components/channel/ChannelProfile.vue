b5fa7b606156c814911497067f871b5e522d63f0
최지우 <qlenfrl101@naver.com>
2024-01-22 16:39:41 +0900

    feat: [#S10P12A402-29] 내 채널 프로필부분 추가

---
+++ frontend/src/components/channel/ChannelProfile.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+  member: Object
+})
+</script>
+
+<template>
+  <div class="channel-profile">
+    <div class="profile-img" :style="{ backgroundImage: `url(${member?.profile})` }"></div>
+    <div class="profile-desc">
+      <span class="name">{{ member?.name }}</span>
+      <span class="desc">{{ member?.desc }}</span>
+      <button class="order">주문관리</button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.channel-profile {
+  @include flex-box(center, flex-start);
+  .profile-img {
+    @include box(10rem, 10rem, $gray, 50%, 0 2rem 0 0, 0);
+  }
+  .name {
+    @include font-factory($fs-5, bold);
+  }
+  .desc {
+    @include font-factory($fs-3, 500);
+    margin: 1rem 0;
+  }
+  .name,
+  .desc {
+    display: block;
+    white-space: pre-line;
+  }
+
+  .order {
+    @include box(fit-content, fit-content, $gray, 10px, 0, 0.3rem 2rem);
+    @include font-factory($fs-1, bold);
+    border: none;
+  }
+}
+</style>
--- frontend/src/views/ChannelView.vue
+++ frontend/src/views/ChannelView.vue
@@ -1,7 +1,21 @@
-<script setup></script>
+<script setup>
+import ChannelProfile from '@/components/channel/ChannelProfile.vue'
+
+const member = {
+  name: '김가을',
+  desc: '안녕하세요! 패션 인플루언서 김가을입니다.\n파시라 한정 세일 진행중이니, 라이브에 많은 관심 부탁드립니다!',
+  profile: '/src/assets/img/test/gaeul.png'
+}
+</script>
 
 <template>
-  <div>내 채널입니다.</div>
+  <div class="channel">
+    <channel-profile :member="member"></channel-profile>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.channel {
+  padding: 2rem 3rem;
+}
+</style>
