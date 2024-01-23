198f041b8272920f12267b9b94be59857114589d
전경향 <wjsrudgid@naver.com>
2024-01-23 15:17:34 +0900

    feat: [#S10P12A402-84] 내 정보 조회

--- backend/src/main/java/org/ssafy/pasila/domain/member/controller/MemberController.java
+++ backend/src/main/java/org/ssafy/pasila/domain/member/controller/MemberController.java
@@ -4,9 +4,12 @@ package org.ssafy.pasila.domain.member.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.ssafy.pasila.domain.member.entity.Member;
 import org.ssafy.pasila.domain.member.repository.MemberRepository;
+
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -16,10 +19,9 @@ public class MemberController {
     private final MemberRepository memberRepository;
 
     // 내 정보 조회
-    @GetMapping("/member")
-    public Member findByMemberEmail() {
-        Member result = memberRepository.findByMemberEmail();
-        return memberRepository.findByMemberEmail();
+    @GetMapping("/personal-info/{memberId}")
+    public Optional<Member> getMember(@PathVariable("memberId") Long memberId) {
+        return memberRepository.findMemberByMemberId(memberId);
     }
 
     // 내 정보 수정
--- backend/src/main/java/org/ssafy/pasila/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/org/ssafy/pasila/domain/member/repository/MemberRepository.java
@@ -1,12 +1,12 @@
 package org.ssafy.pasila.domain.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.ssafy.pasila.domain.member.entity.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    @Query("SELECT m" +
-            "FROM Member m" +
-            "WHERE email = :email")
-    Member findByMemberEmail();
+
+    Optional<Member> findMemberByMemberId(Long memberId);
+
 }
