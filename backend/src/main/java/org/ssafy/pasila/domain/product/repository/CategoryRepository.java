3d13a4ed438b5d16c8c298ce949afe962e9981af
정유경 <76727102+youkyoungJung@users.noreply.github.com>
2024-01-23 10:58:01 +0900

    refactor: [#S10P12A402-106] Merge를 삭제 작업

--- backend/src/main/java/org/ssafy/pasila/domain/product/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package org.ssafy.pasila.domain.product.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.ssafy.pasila.domain.product.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
