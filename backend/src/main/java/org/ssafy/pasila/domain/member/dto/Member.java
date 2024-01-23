99570b613a74800a5c1f2bfa6e06e3a8b4134888
전경향 <wjsrudgid@naver.com>
2024-01-23 10:49:15 +0900

    feat: [#S10P12A402-84] Member DTO 수정

--- backend/src/main/java/org/ssafy/pasila/domain/member/dto/Member.java
+++ backend/src/main/java/org/ssafy/pasila/domain/member/dto/Member.java
@@ -4,7 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.ssafy.pasila.domain.member.entity.Gender;
+import org.ssafy.pasila.domain.member.entity.LoginType;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Builder
@@ -12,7 +15,7 @@ import java.util.Date;
 @AllArgsConstructor
 @Data
 public class Member {
-    private int id;// 사용자id: int
+    private Long id;// 사용자id: int
     private String email;// 이메일: VARCHAR(320) / NN
     private String name;// 이름: VARCHAR(10)
     private String channel;// 채널명: VARCHAR(10)
@@ -20,13 +23,14 @@ public class Member {
     private String password;// 비밀번호: VARCHAR(100)
     private String phone;// 전화번호: VARCHAR(11)
     private String address;// 주소: VARCHAR(50)
-    private String address_detail;// 상세주소: VARCHAR(50)
-    private int gender;// 성별: TINYINT
-    private String birth;// 생년월일: VARCHAR(8)
+    private String addressDetail;// 상세주소: VARCHAR(50)
+    private Gender gender;// 성별: TINYINT
+    private Date birth;// 생년월일: VARCHAR(8)
     private String bank;// 은행: VARCHAR(10)
     private String account;// 계좌번호: VARCHAR(20)
     private String profile;// 프로필사진: VARCHAR(2083)
-    private Date created_at;// 가입일: datetime
-    private String login_type;// 로그인타입: enum("google", "kakao", "naver", "email")
+    private LocalDateTime createdAt;// 가입일: datetime
+    private LocalDateTime deletedAt;// 탈퇴일
+    private LoginType loginType;// 로그인타입: enum("google", "kakao", "naver", "email")
     private String token;// 토큰: VARCHAR(256)
 }
