ac8995c4479d70612bdbf45995d5074e3b5c9154
최지우 <qlenfrl101@naver.com>
2024-01-26 18:35:22 +0900

    refactor: [#S10P12A402-30] table 컴포넌트 분리

--- frontend/src/views/ChannelOrders.vue
+++ frontend/src/views/ChannelOrders.vue
@@ -1,4 +1,5 @@
 <script setup>
+import ProductTable from '@/components/channel/ProductTable.vue'
 const myProductList = [
   {
     name: '겨울에 필수! 앙고라 니트',
@@ -96,78 +97,7 @@ const myProductList = [
       <h3 class="subtitle">내 판매 상품의 주문, 배송 상태를 확인하고 관리하세요.</h3>
     </section>
     <section class="bottom">
-      <table>
-        <thead>
-          <tr>
-            <th>상품 정보</th>
-            <th>등록일</th>
-            <th>재고</th>
-            <th>주문 리스트</th>
-          </tr>
-        </thead>
-        <tbody>
-          <template v-for="(item, index) in myProductList" :key="index">
-            <tr>
-              <td class="product">
-                <div class="thumbnail" :style="{ backgroundImage: `url(${item.thumbnail})` }"></div>
-                <div class="right">
-                  <span class="name">{{ item.name }}</span>
-                  <div class="price-line">
-                    <span class="price">{{
-                      item.ProductOptions[0].price.toLocaleString('kr-KR')
-                    }}</span>
-                    <span class="discount-price">{{
-                      item.ProductOptions[0].discountPrice.toLocaleString('kr-KR')
-                    }}</span>
-                  </div>
-                </div>
-              </td>
-              <td class="date">{{ item.createdAt.substring(0, 10) }}</td>
-              <td class="option-stock">
-                <template v-for="(option, index2) in item.ProductOptions" :key="index2">
-                  <div>
-                    <span class="name">{{ option.name }} : </span>
-                    <span class="stock">{{ option.stock === 0 ? '품절' : option.stock }}</span>
-                  </div>
-                </template>
-              </td>
-              <td class="list-btn">
-                <span>
-                  <font-awesome-icon icon="fa-solid fa-chevron-down" />
-                </span>
-              </td>
-            </tr>
-            <tr>
-              <td colspan="4">
-                <table style="table-layout: fixed; width: 100%">
-                  <thead>
-                    <tr>
-                      <th>구매자</th>
-                      <th>배송지</th>
-                      <th>구매 옵션</th>
-                      <th>배송 상태</th>
-                      <th>주문 취소</th>
-                      <th>결제 금액</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <template v-for="(order, index3) in item.Orders" :key="index3">
-                      <tr>
-                        <td>{{ order.name }}</td>
-                        <td>{{ order.address }}</td>
-                        <td>{{ order.productOption.name }}<br />{{ order.order_cnt }}개</td>
-                        <td>{{ order.status }}</td>
-                        <td><button>주문 취소</button></td>
-                        <td>{{ order.price }}</td>
-                      </tr>
-                    </template>
-                  </tbody>
-                </table>
-              </td>
-            </tr>
-          </template>
-        </tbody>
-      </table>
+      <product-table :products="myProductList"></product-table>
     </section>
   </div>
 </template>
@@ -189,62 +119,6 @@ const myProductList = [
   .bottom {
     @include flex-box(flex-start, center);
     padding-top: 3rem;
-    table {
-      border-collapse: collapse;
-      tr {
-        // border: 1px solid black;
-        border-bottom: 1px solid $gray;
-      }
-      th {
-        padding: 1.5rem 0;
-        //   border-bottom: 1px solid $gray;
-      }
-      tbody {
-        td {
-        }
-        .product {
-          @include flex-box(flex-start, flex-start);
-          padding: 2.5rem 5rem;
-          .thumbnail {
-            @include box(10rem, 5rem, 0, 1.25rem, null, null);
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-            box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-          }
-          .right {
-            padding: 0 1rem;
-            .name {
-              @include font-factory($fs-3, 500);
-            }
-            .price-line {
-              margin-top: 1rem;
-              .price {
-                @include font-factory($fs-2, 500);
-                text-decoration: line-through;
-              }
-              .discount-price {
-                @include font-factory($fs-3, bold, $main);
-                margin: 0 1rem;
-              }
-            }
-          }
-        }
-        .date {
-          padding: 0 5rem;
-        }
-        .option-stock {
-          padding: 0 5rem;
-        }
-        .list-btn {
-          text-align: center;
-          padding: 0 5rem;
-          span {
-            cursor: pointer;
-          }
-        }
-      }
-    }
   }
 }
 </style>
