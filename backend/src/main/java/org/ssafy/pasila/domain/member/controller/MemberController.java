89013eee181764e174c2baac83a4cea651a3429f
전경향 <wjsrudgid@naver.com>
2024-01-29 15:18:01 +0900

    fix: [#S10P12A402-85] PersonalInfoRepository 경로 수정

--- backend/src/main/java/org/ssafy/pasila/domain/member/controller/MemberController.java
+++ backend/src/main/java/org/ssafy/pasila/domain/member/controller/MemberController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.ssafy.pasila.domain.member.dto.PersonalInfo.PersonalInfoResponse;
+import org.ssafy.pasila.domain.member.dto.response.PersonalInfoResponse;
 import org.ssafy.pasila.domain.member.repository.PersonalInfoRepository;
 
 import java.util.Optional;
