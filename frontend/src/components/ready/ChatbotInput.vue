b6592eab5823947a769610fa11e50be99b78d672
parkjiwon <wldnjs11234@naver.com>
2024-01-29 17:31:34 +0900

    feat: [#S10P10A402-111] 챗봇페이지 구현중

--- frontend/src/components/ready/ChatbotInput.vue
+++ frontend/src/components/ready/ChatbotInput.vue
@@ -1,9 +1,67 @@
-<script setup></script>
+<script setup>
+import { ref, watch } from 'vue'
+
+const question = ref('')
+const answer = ref('')
+const emit = defineEmits(['data'])
+const saveQuestion = () => {
+  emit('data', { q: question, a: answer })
+  console.log(emit)
+}
+</script>
 
 <template>
-  <div>
-    <div>챗봇 박스이다.</div>
+  <div class="chatbot-box">
+    <div class="text-box">
+      <label for="question">질문</label>
+      <input
+        id="question"
+        type="text"
+        placeholder="질문을 입력해주세요."
+        class="input-box"
+        v-model="question"
+      />
+    </div>
+    <div class="text-box">
+      <label for="answer">답변</label>
+      <input
+        id="answer"
+        type="text"
+        placeholder="답변을 입력해주세요."
+        class="input-box"
+        v-model="answer"
+      />
+    </div>
+    <button class="save-btn" @click="saveQuestion()">질문 저장</button>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.chatbot-box {
+  @include box(100%, 100%, white, 0.3rem, 0.2rem, 0.5rem);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+
+  .text-box {
+    width: 100%;
+    display: flex;
+    padding: 0.5rem;
+  }
+  label {
+    margin: 0 1rem;
+  }
+  .input-box {
+    @include box(80%, 100%, $light-gray, 0.3rem, 0.1rem, 0.1rem);
+    border: none;
+    outline: none;
+  }
+  .save-btn {
+    @include box(30%, 10%, white, 0.3rem, 0.1rem, 0.2rem);
+    cursor: pointer;
+    color: $main;
+    border: 1px solid $main;
+  }
+}
+</style>
--- frontend/src/views/ReadyChatbotView.vue
+++ frontend/src/views/ReadyChatbotView.vue
@@ -6,13 +6,43 @@ import { ref } from 'vue'
 
 const step = ref('chatbot')
 const nextStep = ref('schedule')
+const questions = ref([
+  {
+    q: '',
+    a: ''
+  }
+])
+
+const addQuestion = () => {
+  questions.value.push({ q: '', a: '' })
+}
+
+const removeQuestion = (i) => {
+  console.log(questions.value)
+  console.log(i)
+  const temp = questions.value.splice(i, 1)
+  console.log(temp)
+  console.log(questions.value)
+}
 </script>
 
 <template>
   <div class="container">
     <steps :data="step" />
-    <div>
-      <chatbot-input />
+    <div class="chatbot-body">
+      <div class="faq">FAQ</div>
+      <div v-for="(question, index) in questions" :key="index" class="chatbot-input">
+        <chatbot-input
+          @data="
+            (q, a) => {
+              question.q = q
+              question.a = a
+            }
+          "
+        />
+        <button class="remove-btn" @click="removeQuestion(index)">질문 삭제</button>
+      </div>
+      <button @click="addQuestion" class="add-btn">+ 질문 추가하기</button>
     </div>
     <next-button :data="nextStep" />
   </div>
@@ -21,5 +51,39 @@ const nextStep = ref('schedule')
 <style lang="scss" scoped>
 .container {
   @include box(95%, 100%, none, 0.3rem, 0.8rem, 0.5rem);
+
+  .chatbot-body {
+    @include box(100%, 100%, none, 0, 0, 0);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .faq {
+      @include font-factory($fs-4, bold, $main);
+      margin-bottom: 1rem;
+    }
+    .add-btn {
+      @include box(53%, 100%, white, 0.3rem, 0.1rem, 0.1rem);
+      cursor: pointer;
+      border: 1px solid $main;
+      color: $main;
+    }
+
+    .chatbot-input {
+      @include box(50%, 100%, white, 0.3rem, 0.2rem, 0.5rem);
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
+      border: 1px solid $light-dark;
+
+      .remove-btn {
+        @include box(30%, 10%, white, 0.3rem, 0.1rem, 0.2rem);
+        cursor: pointer;
+        color: $main;
+        border: 1px solid $main;
+      }
+    }
+  }
 }
 </style>
