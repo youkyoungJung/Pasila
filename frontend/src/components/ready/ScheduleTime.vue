7e06adc54332222e04cdab28bdd2240ac9797bdf
parkjiwon <wldnjs11234@naver.com>
2024-01-31 11:20:04 +0900

    feat: [#S10P12A402-109] 라이브예약 페이지 구현

---
+++ frontend/src/components/ready/ScheduleTime.vue
@@ -0,0 +1,145 @@
+<script setup>
+const emit = defineEmits(['getHour', 'getMinute', 'getAM'])
+</script>
+
+<template>
+  <div class="select-time">
+    <div class="vc-time-select-group">
+      <div class="vc-base-select">
+        <select @change="$emit('getAM', $event.target.value)" class="select-am">
+          <option value="오전">오전</option>
+          <option value="오후">오후</option>
+        </select>
+        <select
+          modelmodifiers="[object Object]"
+          class="vc-time-select-hours vc-focus vc-align-right"
+          @change="$emit('getHour', $event.target.value)"
+        >
+          <option value="1">01</option>
+          <option value="2">02</option>
+          <option value="3">03</option>
+          <option value="4">04</option>
+          <option value="5">05</option>
+          <option value="6">06</option>
+          <option value="7">07</option>
+          <option value="8">08</option>
+          <option value="9">09</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+        </select>
+      </div>
+      <div class="vc-base-select">
+        <select
+          modelmodifiers="[object Object]"
+          class="vc-time-select-minutes vc-focus vc-align-left"
+          @change="$emit('getMinute', $event.target.value)"
+        >
+          <option value="0">00</option>
+          <option value="1">01</option>
+          <option value="2">02</option>
+          <option value="3">03</option>
+          <option value="4">04</option>
+          <option value="5">05</option>
+          <option value="6">06</option>
+          <option value="7">07</option>
+          <option value="8">08</option>
+          <option value="9">09</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+          <option value="14">14</option>
+          <option value="15">15</option>
+          <option value="16">16</option>
+          <option value="17">17</option>
+          <option value="18">18</option>
+          <option value="19">19</option>
+          <option value="20">20</option>
+          <option value="21">21</option>
+          <option value="22">22</option>
+          <option value="23">23</option>
+          <option value="24">24</option>
+          <option value="25">25</option>
+          <option value="26">26</option>
+          <option value="27">27</option>
+          <option value="28">28</option>
+          <option value="29">29</option>
+          <option value="30">30</option>
+          <option value="31">31</option>
+          <option value="32">32</option>
+          <option value="33">33</option>
+          <option value="34">34</option>
+          <option value="35">35</option>
+          <option value="36">36</option>
+          <option value="37">37</option>
+          <option value="38">38</option>
+          <option value="39">39</option>
+          <option value="40">40</option>
+          <option value="41">41</option>
+          <option value="42">42</option>
+          <option value="43">43</option>
+          <option value="44">44</option>
+          <option value="45">45</option>
+          <option value="46">46</option>
+          <option value="47">47</option>
+          <option value="48">48</option>
+          <option value="49">49</option>
+          <option value="50">50</option>
+          <option value="51">51</option>
+          <option value="52">52</option>
+          <option value="53">53</option>
+          <option value="54">54</option>
+          <option value="55">55</option>
+          <option value="56">56</option>
+          <option value="57">57</option>
+          <option value="58">58</option>
+          <option value="59">59</option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.select-time {
+  @include box(100%, 7rem, white, 0.625rem, 0, 0);
+  .vc-time-select-group {
+    @include box(100%, 100%, white, 0, 0, 0);
+    @include flex-box();
+    .select-am {
+      width: 6rem;
+      height: 4rem;
+      flex-shrink: 0;
+      border: none;
+      border-radius: 0.625rem;
+      background-color: #d9d9d9;
+      outline: none;
+      margin-right: 1rem;
+
+      text-align: center;
+      font-family: 'Noto Sans KR';
+      font-size: 1.5rem;
+      font-style: normal;
+      font-weight: 500;
+      line-height: normal;
+    }
+    select {
+      width: 9rem;
+      height: 4rem;
+      flex-shrink: 0;
+      border: none;
+      border-radius: 0.625rem;
+      background-color: #d9d9d9;
+      margin-left: 0.5rem;
+
+      text-align: center;
+      font-family: 'Noto Sans KR';
+      font-size: 1.5rem;
+      font-style: normal;
+      font-weight: 500;
+      line-height: normal;
+    }
+  }
+}
+</style>
--- frontend/src/views/ReadyScheduleView.vue
+++ frontend/src/views/ReadyScheduleView.vue
@@ -1,134 +1,48 @@
 <script setup>
 import steps from '@/components/ready/Steps.vue'
+import ScheduleCalendar from '@/components/ready/ScheduleCalendar.vue'
+import ScheduleTime from '@/components/ready/ScheduleTime.vue'
 import 'v-calendar/style.css'
 
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 
 const step = ref('schedule')
-const date = ref('')
-const realDate = ref('')
-const trimDate = () => {
-  if (date.value != '') realDate.value = date.value.toLocaleDateString()
+
+const liveTitle = ref('')
+const date = ref(new Date())
+const apm = ref(new Date().getHours >= 12 ? '오후' : '오전')
+const hour = ref(new Date().getHours())
+const minute = ref(new Date().getMinutes())
+
+const reserveLive = () => {
+  //라이브 예약완료 버튼 기능 추가하기
 }
-const hour = ref('00')
-const minute = ref('00')
 </script>
 
 <template>
   <div class="container">
     <steps :data="step" />
+    <input class="title-input" placeholder="라이브 제목을 입력해주세요!" v-model="liveTitle" />
     <div class="body">
-      <div class="calendar">
-        <v-date-picker v-model="date" @click="trimDate" />
+      <div class="body-left">
+        <schedule-calendar :date="date" @getDate="(v) => (date = v)" />
       </div>
-      <div class="schedule-time">
-        <div class="vc-time-picker vc-attached">
-          <div class="vc-time-select-group">
-            <div class="vc-base-select">
-              <select
-                modelmodifiers="[object Object]"
-                class="vc-time-select-hours vc-focus vc-align-right"
-                v-model="hour"
-              >
-                <option value="0">00</option>
-                <option value="1">01</option>
-                <option value="2">02</option>
-                <option value="3">03</option>
-                <option value="4">04</option>
-                <option value="5">05</option>
-                <option value="6">06</option>
-                <option value="7">07</option>
-                <option value="8">08</option>
-                <option value="9">09</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-                <option value="13">1</option>
-                <option value="14">2</option>
-                <option value="15">3</option>
-                <option value="16">4</option>
-                <option value="17">5</option>
-                <option value="18">6</option>
-                <option value="19">7</option>
-                <option value="20">8</option>
-                <option value="21">9</option>
-                <option value="22">10</option>
-                <option value="23">11</option></select
-              ><!----><!---->
+      <div class="body-right">
+        <schedule-time
+          @getAM="(v) => (apm = v)"
+          @getHour="(v) => (hour = v)"
+          @getMinute="(v) => (minute = v)"
+        />
+        <div class="show-time">
+          <div class="time-title">라이브 방송 예정 시간은</div>
+          <div class="time-body">
+            <div v-if="date != ''">
+              {{ date.getFullYear() }}년 {{ date.getMonth() + 1 }}월 {{ date.getDate() }}일
             </div>
-            <span class="vc-time-colon">:</span>
-            <div class="vc-base-select">
-              <select
-                modelmodifiers="[object Object]"
-                class="vc-time-select-minutes vc-focus vc-align-left"
-                v-model="minute"
-              >
-                <option value="0">00</option>
-                <option value="1">01</option>
-                <option value="2">02</option>
-                <option value="3">03</option>
-                <option value="4">04</option>
-                <option value="5">05</option>
-                <option value="6">06</option>
-                <option value="7">07</option>
-                <option value="8">08</option>
-                <option value="9">09</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-                <option value="13">13</option>
-                <option value="14">14</option>
-                <option value="15">15</option>
-                <option value="16">16</option>
-                <option value="17">17</option>
-                <option value="18">18</option>
-                <option value="19">19</option>
-                <option value="20">20</option>
-                <option value="21">21</option>
-                <option value="22">22</option>
-                <option value="23">23</option>
-                <option value="24">24</option>
-                <option value="25">25</option>
-                <option value="26">26</option>
-                <option value="27">27</option>
-                <option value="28">28</option>
-                <option value="29">29</option>
-                <option value="30">30</option>
-                <option value="31">31</option>
-                <option value="32">32</option>
-                <option value="33">33</option>
-                <option value="34">34</option>
-                <option value="35">35</option>
-                <option value="36">36</option>
-                <option value="37">37</option>
-                <option value="38">38</option>
-                <option value="39">39</option>
-                <option value="40">40</option>
-                <option value="41">41</option>
-                <option value="42">42</option>
-                <option value="43">43</option>
-                <option value="44">44</option>
-                <option value="45">45</option>
-                <option value="46">46</option>
-                <option value="47">47</option>
-                <option value="48">48</option>
-                <option value="49">49</option>
-                <option value="50">50</option>
-                <option value="51">51</option>
-                <option value="52">52</option>
-                <option value="53">53</option>
-                <option value="54">54</option>
-                <option value="55">55</option>
-                <option value="56">56</option>
-                <option value="57">57</option>
-                <option value="58">58</option>
-                <option value="59">59</option></select
-              ><!----><!---->
-            </div>
+            <div>{{ apm }} {{ hour }}시{{ minute }}분 입니다.</div>
           </div>
         </div>
-        <div>{{ realDate }}</div>
-        <div>{{ hour }}:{{ minute }}</div>
+        <button @click="reserveLive" class="reserve-btn">예약하기</button>
       </div>
     </div>
   </div>
@@ -137,19 +51,76 @@ const minute = ref('00')
 <style lang="scss" scoped>
 .container {
   @include box(95%, 100%, none, 0.3rem, 0.8rem, 0.5rem);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
+  .title-input {
+    width: 80%;
+    height: 3rem;
+    border: #d9d9d9;
+    border-radius: 0.625rem;
+    margin-top: 2rem;
+    padding-left: 0.5rem;
+    background-color: #d9d9d9;
+    outline: none;
+    font-family: 'Noto Sans KR';
+    font-size: 1.3rem;
+    font-weight: 500;
+  }
   .body {
-    @include box(100%, 100%, red, 0, 0.3rem, 0.1rem);
-    @include flex-box($align: flex-start, $justify: space-evenly);
+    @include box(100%, 100%, white, 0, 0.3rem, 0.1rem);
+    display: flex;
+    justify-content: space-around;
     border-radius: 0.5rem;
 
-    .calendar {
-      @include box(100%, 100%, blue, 0, 0.1rem, 0.2rem);
+    .body-left {
+      @include box(40%, 100%, white, 0, 0.1rem, 0.2rem);
       display: flex;
-      justify-content: center;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
     }
-    .schedule-time {
-      @include box(100%, 100%, pink, 0, 0.1rem, 0.2rem);
+
+    .body-right {
+      @include box(40%, 100%, white, 0, 0.1rem, 0.2rem);
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      .show-time {
+        width: 28rem;
+        height: 18rem;
+        flex-shrink: 0;
+        border-radius: 0.625rem;
+        border: 5px solid $main;
+        color: #000;
+
+        text-align: center;
+        font-family: 'Noto Sans KR';
+        font-size: 2.5rem;
+        font-weight: 500;
+        .time-title {
+          @include font-factory(1.5rem, 500);
+          display: flex;
+          justify-content: flex-start;
+          padding: 0.3rem;
+          margin-left: 1.5rem;
+          margin-top: 1.5rem;
+          margin-bottom: 2rem;
+        }
+      }
+
+      .reserve-btn {
+        @include font-factory(1.4rem, 500, white);
+        width: 28rem;
+        height: 4rem;
+        border-radius: 0.625rem;
+        background: $main;
+        border: none;
+        cursor: pointer;
+        margin-top: 3rem;
+      }
     }
   }
 }
