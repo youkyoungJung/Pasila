e05daa10a41e97f5cbe101fd17102eec40b8ac4c
parkjiwon <wldnjs11234@naver.com>
2024-02-01 10:38:29 +0900

    refactor: [#S10P12A402-157] 로그인, 회원가입 페이지 컴포넌트화

--- frontend/src/views/JoinView.vue
+++ frontend/src/views/JoinView.vue
@@ -1,246 +1,291 @@
-<template>
-    <div class="container">
-        <div class="header">회원가입</div>
-        <div class="content">
-            <section class="profile">
-                <div>
-                    <div v-if="user.profile != ''">
-                        <img :src="user.profile" id="profileImg" class="profile-img">
-                    </div>
-                    <div v-else="user.profile === ''">
-                        <font-awesome-icon icon="fa-regular fa-user" class="profile-img" />
-                    </div>
-                </div>
-                <label for="file">프로필 사진 등록</label>
-                <input type="file" id="file" @change="uploadImg" accept="image/*" class="profile-choose" />
-            </section>
-            <section class="short-type">
-                <label for="email">이메일</label>
-                <div class="input-area">
-                    <input type="email" id="email" v-model="user.email" placeholder="메일을 입력하세요" class="short-input">
-                    <input type="submit" value="중복확인" class="input-btn">
-                </div>
-            </section>
-            <section class="long-type">
-                <label for="name">이름</label>
-                <input type="text" id="name" v-model="user.name" placeholder="이름을 입력하세요" class="long-input">
-            </section>
-            <section class="short-type">
-                <label for="channelName">채널명</label>
-                <div class="input-area">
-                    <input type="text" id="channelName" v-model="user.channelName" placeholder="채널명을 입력하세요" class="short-input">
-                    <input type="submit" value="중복확인" class="input-btn">
-                </div>
-            </section>
-            <section class="long-type">
-                <label for="password">비밀번호</label>
-                <input type="password" id="password" v-model="user.password" placeholder="비밀번호를 입력하세요" class="long-input">
-            </section>
-            <section class="long-type">
-                <label for="passwordCheck">비밀번호 확인</label>
-                <input type="password" id="passwordCheck" placeholder="비밀번호를 한 번 더 입력하세요" class="long-input">
-                <div style="margin: 2px; color: #ff495c; font-size: 10px;">
-                    비밀번호 맞는지 확인하는 텍스트
-                </div>
-            </section>
-            <section class="short-type">
-                <label for="phoneNumber">휴대폰</label>
-                <div class="input-area">
-                    <input id="phoneNumber" type="number" v-model="user.phone" placeholder="휴대폰 번호를 입력하세요" class="short-input">
-                    <input type="submit" value="번호받기" class="input-btn">
-                </div>
-            </section>
-            <section class="short-type">
-                <label for="phoneCheck">인증번호 확인</label>
-                <div class="input-area">
-                    <input id="phoneCheck" type="number" placeholder="인증번호를 입력하세요" class="short-input">
-                    <input type="submit" value="인증확인" class="input-btn">
-                </div>
-                <div style="margin: 2px; color: #ff495c; font-size: 10px;" >
-                    인증번호 확인하는 텍스트
-                </div>
-            </section>
-            <section class="short-type">
-                <label for="address">주소</label>
-                <div class="input-area">
-                    <input type="text" id="address" v-model="user.address" placeholder="주소를 입력하세요" class="short-input">
-                    <input type="submit" value="주소검색" class="input-btn">
-                </div>
-            </section>
-            <section class="long-type">
-                <label for="detailAddress">상세주소</label>
-                <input type="text" id="detailAddress" v-model="user.detailAddress" placeholder="상세주소를 입력하세요" class="long-input">
-            </section>
-            <section class="long-type">
-                <form>
-                    <label for="gender">성별</label>
-                    <div class="radio" id="gender">
-                        <label><input type="radio" name="성별" value="남성" v-model="user.gender">남성</label>
-                        <label><input type="radio" name="성별" value="여성" v-model="user.gender">여성</label>
-                        <label><input type="radio" name="성별" value="선택안함" v-model="user.gender">선택안함</label>
-                    </div>
-                </form>
-            </section>
-            <section class="long-type">
-                <label for="birth">생년월일</label>
-                <input type="date" id="birth" v-model="user.birth" class="long-input">
-            </section>
-            <button @click="join" class="join">회원가입</button>
-        </div>
-    </div>
-</template>
-
 <script setup>
-import router from '@/router';
-import { ref } from 'vue';
+import router from '@/router'
+import { ref } from 'vue'
+import VLongInput from '@/components/common/VLongInput.vue'
+import VShortInput from '@/components/common/VShortInput.vue'
 
 const user = ref({
-    profile: '',
-    email: '',
-    name: '',
-    channelName: '',
-    password: '',
-    phone: '',
-    address: '',
-    detailAddress: '',
-    gender: '',
-    birth: '',
-});
+  profile: '',
+  email: '',
+  name: '',
+  channelName: '',
+  password: '',
+  phone: '',
+  address: '',
+  detailAddress: '',
+  gender: '',
+  birth: ''
+})
 
+const longData = ref({
+  name: {
+    title: '이름',
+    type: 'text'
+  },
+  password: {
+    title: '비밀번호',
+    type: 'password'
+  },
+  passwordCheck: {
+    title: '비밀번호 확인',
+    type: 'password'
+  },
+  detailAddress: {
+    title: '상세주소',
+    type: 'password'
+  },
+  birth: {
+    title: '생년월일',
+    type: 'date'
+  }
+})
+
+const shortData = ref({
+  email: {
+    title: '이메일',
+    type: 'email',
+    text: '중복확인'
+  },
+  channel: {
+    title: '채널명',
+    type: 'text',
+    text: '중복확인'
+  },
+  phone: {
+    title: '휴대폰번호',
+    type: 'number',
+    text: '번호받기'
+  },
+  phoneCheck: {
+    title: '인증번호 확인',
+    type: 'number',
+    text: '인증확인'
+  },
+  address: {
+    title: '주소',
+    type: 'text',
+    text: '주소검색'
+  }
+})
 const uploadImg = () => {
-    const reader = new FileReader();
-    reader.onload = function(e) {
-        user.value.profile = e.target.result;
-    }
-    reader.readAsDataURL(file.files[0]);
-    console.log(user.value);
+  const reader = new FileReader()
+  reader.onload = function (e) {
+    user.value.profile = e.target.result
+  }
+  reader.readAsDataURL(file.files[0])
 }
-
-    
 
 const join = () => {
-    //user.value 백에 넘겨주고 메인페이지 가야함
-    if (user.value.birth != '') {
-        user.value.birth += ' 00:00:00';
-    }
-    console.log(user.value)
-    router.push("/");
+  //user.value 백에 넘겨주기
+  if (user.value.birth != '') {
+    user.value.birth += ' 00:00:00'
+  }
+  router.push('/')
 }
-
 </script>
+
+<template>
+  <div class="container">
+    <div class="header">회원가입</div>
+    <div class="content">
+      <section class="profile">
+        <div>
+          <div v-if="user.profile != ''">
+            <img :src="user.profile" id="profileImg" class="profile-img" />
+          </div>
+          <div v-else="user.profile === ''">
+            <font-awesome-icon icon="fa-regular fa-user" class="profile-img" />
+          </div>
+        </div>
+        <label for="file">프로필 사진 등록</label>
+        <input type="file" id="file" @change="uploadImg" accept="image/*" class="profile-choose" />
+      </section>
+      <section class="userInfo">
+        <v-short-input :data="shortData.email" @getData="(e) => (user.email = e)" />
+      </section>
+      <section class="userInfo">
+        <v-long-input :data="longData.name" @getData="(e) => (user.name = e)" />
+      </section>
+      <section class="userInfo">
+        <v-short-input :data="shortData.channel" @getData="(e) => (user.channelName = e)" />
+      </section>
+      <section class="userInfo">
+        <v-long-input :data="longData.password" @getData="(e) => (user.password = e)" />
+      </section>
+      <section class="userInfo">
+        <v-long-input :data="longData.passwordCheck" @getData="(e) => (user.passwordCheck = e)" />
+        <div class="check-text">비밀번호 맞는지 확인하는 텍스트</div>
+      </section>
+      <section class="userInfo">
+        <v-short-input :data="shortData.phone" @getData="(e) => (user.phone = e)" />
+      </section>
+      <section class="userInfo">
+        <v-short-input :data="shortData.phoneCheck" />
+        <div class="check-text">인증번호 확인하는 텍스트</div>
+      </section>
+      <section class="userInfo">
+        <v-short-input :data="shortData.address" @getData="(e) => (user.address = e)" />
+      </section>
+      <section class="userInfo">
+        <v-long-input :data="longData.detailAddress" @getData="(e) => (user.detailAddress = e)" />
+      </section>
+      <section class="gender">
+        <form>
+          <label for="gender" class="gender-title">성별</label>
+          <div class="radio" id="gender">
+            <label class="gender-option"
+              ><input type="radio" name="성별" value="남성" v-model="user.gender" />남성</label
+            >
+            <label class="gender-option"
+              ><input type="radio" name="성별" value="여성" v-model="user.gender" />여성</label
+            >
+            <label class="gender-option"
+              ><input
+                type="radio"
+                name="성별"
+                value="선택안함"
+                v-model="user.gender"
+              />선택안함</label
+            >
+          </div>
+        </form>
+      </section>
+      <section class="userInfo">
+        <label for="input" class="label">생년월일</label>
+        <input type="date" value="YYYY/MM/DD" v-model="user.birth" class="birth" />
+      </section>
+      <div class="join-btn">
+        <button @click="join" class="join">회원가입</button>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style lang="scss" scoped>
 .container {
-    @include box(500px, 100%, white, 10px, 5px, 5px);
-    border: 2px solid whitesmoke;
-    @include flex-box($direction: column);
-    @include drop-shadow;
-    vertical-align: middle;
-    margin-left: auto;
-    margin-right: auto;
-    .header {
-        margin-top: 20px;
-        @include font-factory($fs-3, bold);
+  @include box(500px, 90%, white, 0.625rem, 1rem, 0.1rem);
+  @include font-factory(13px, bold);
+  @include flex-box($direction: column);
+  @include drop-shadow;
+  border: 2px solid #d9d9d9;
+  margin-left: auto;
+  margin-right: auto;
+  .header {
+    margin: 2rem 0;
+    @include font-factory($fs-3, bold);
+  }
+  .content {
+    @include box(90%, 90%, none, 0, 0, 0);
+    @include font-factory(13px, bold);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .profile {
+      @include box(100%, 10%, white, 0, 0.2rem, 0.2rem);
+      @include flex-box($direction: column);
+
+      .profile-img {
+        @include box(3.5rem, 3.5rem, none, 50%, 0, 0);
+      }
+
+      label {
+        @include font-factory(10px, null);
+      }
+      .profile-choose {
+        @include font-factory(13px, null);
+        width: 0;
+        height: 0;
+        padding: 0;
+        overflow: hidden;
+        border: 0;
+      }
     }
-    .content {
-        @include box(90%, 90%, none, 0, 10px, 10px);
-        @include flex-box($direction: column, $justify: none);
-        @include font-factory(13px, bold);
-        
-        .profile {
-            @include box(100%, 10%, white, 0, 2px, 5px);
-            @include flex-box($direction: column);
-            
-            .profile-img {
-                @include box(60px, 60px, none, 50%, 2px, 5px);
-                
-                
-            }
+    .userInfo {
+      @include box(95%, 10%, null, 0, 0, 0);
+      display: flex;
+      flex-direction: column;
+      align-items: center;
 
-            label {
-                @include font-factory(10px, null);
-            }
-            .profile-choose {
-                @include font-factory(13px, null);
-                width: 0;
-                height: 0;
-                padding: 0;
-                overflow: hidden;
-                border: 0;
-                // text-align: flex-end;
-                // border: 1px solid $dark;
-                // background-color: white;
-                // margin-left: 10px;
-                // padding: 5px;
-            }
-            
-        }
-        .long-type {
-            @include box(90%, 10%, null, 0, 2px, 5px);
-
-            label {
-                text-align: flex-start;
-            }
-
-            .long-input {
-                @include box(95%, 50%, whitesmoke, 0, null, 10px);
-                border: none;
-                margin-top: 5px;
-            }
-
-            
-        }
-
-        .short-type {
-            @include box(90%, 10%, null, 0, 2px, 5px);
-            .label {
-                text-align: flex-start;
-            }
-
-            .input-area {
-                @include box(100%, 60%, null, 0, 2px, null);
-                @include flex-box($justify: space-between);
-
-                margin-top: 5px;
-
-                .short-input {
-                    @include box(80%, 90%, whitesmoke, 0, null, 10px);
-                    border: none;
-                }
-    
-                .input-btn {
-                    @include font-factory(13px, null);
-                    text-align: flex-end;
-                    border: 1px solid $dark;
-                    background-color: white;
-                    margin-left: 10px;
-                    padding: 5px;
-                }
-
-            }
-        }
-
-    .radio {
-        @include flex-box($justify: flex-start);
-        @include font-factory(11px, null);
-
-        label {
-            margin: 10px 10px 0px 10px;
-        }
-    }
-
-
-    .join {
+      .label {
         width: 90%;
-        height: 43px;
-        margin: 10px;
+        display: flex;
+        justify-content: flex-start;
+        text-align: flex-start;
+      }
+
+      .birth {
+        @include box(90%, 2.5rem, whitesmoke, 0, 0, 0);
         border: none;
-        background-color: $main;
-        color: white;
+        margin-top: 0.2rem;
+        padding-left: 0.5rem;
+        outline: none;
+      }
+      ::-webkit-calendar-picker-indicator {
         cursor: pointer;
+        width: 10%;
+      }
     }
 
+    .check-text {
+      margin: 2px;
+      color: $main;
+      font-size: 0.7rem;
+      width: 90%;
+      display: flex;
+      justify-content: flex-start;
+      text-align: flex-start;
+      margin-bottom: 0.3rem;
     }
 
+    .gender {
+      width: 95%;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      margin-bottom: 0.2rem;
+
+      form {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        .gender-title {
+          width: 90%;
+          display: flex;
+          justify-content: flex-start;
+        }
+        .radio {
+          @include font-factory(0.7rem, null);
+          width: 100%;
+          display: flex;
+          justify-content: space-evenly;
+        }
+
+        .gender-option {
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+          margin-right: 0.3rem;
+          cursor: pointer;
+        }
+      }
+    }
+    .join-btn {
+      width: 88%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      border-top: 1px solid $dark;
+      margin: 2rem;
+      padding-top: 1rem;
+      .join {
+        @include box(100%, 3rem, $main, 0, 1rem, 0);
+        @include font-factory($fs-1, null, white);
+        border: none;
+        cursor: pointer;
+      }
+    }
+  }
 }
 </style>
