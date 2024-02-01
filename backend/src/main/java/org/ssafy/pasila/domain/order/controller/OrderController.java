83ab4a8273fc82672cefcab85ab02d822e205258
정유경 <76727102+youkyoungJung@users.noreply.github.com>
2024-02-01 09:59:14 +0900

    refactor: [#feat:S10P12A402-191] 메소드 명 변경

--- backend/src/main/java/org/ssafy/pasila/domain/order/controller/OrderController.java
+++ backend/src/main/java/org/ssafy/pasila/domain/order/controller/OrderController.java
@@ -6,9 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.ssafy.pasila.domain.apihandler.ApiCommonResponse;
+import org.ssafy.pasila.domain.order.dto.OrderDto;
 import org.ssafy.pasila.domain.order.dto.OrderFormDto;
 import org.ssafy.pasila.domain.order.service.OrderService;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,11 +21,21 @@ public class OrderController {
 
     @Operation(summary = "Save order", description = "주문을 등록한다.")
     @PostMapping
-    public ApiCommonResponse<?> createProduct(@RequestBody OrderFormDto orderFormDto) throws IOException {
+    public ApiCommonResponse<?> createOrder(@RequestBody OrderFormDto orderFormDto) throws IOException {
 
         Long orderId = orderService.saveOrder(orderFormDto);
 
         return ApiCommonResponse.successResponse(HttpStatus.OK.value(), orderId);
+
+    }
+
+    @Operation(summary = "Get Order List", description = "[구매자]주문 내역을 조회")
+    @GetMapping("/{id}")
+    public ApiCommonResponse<?> getOrderList(@PathVariable Long id) throws IOException {
+
+        List<OrderDto> orderList = orderService.getOrderList(id);
+
+        return ApiCommonResponse.successResponse(HttpStatus.OK.value(), orderList);
 
     }
 
