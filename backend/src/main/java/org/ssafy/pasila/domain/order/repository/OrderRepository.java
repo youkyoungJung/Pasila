021b070aa4a3df4b50defaf53ca18d50eef688ea
정유경 <76727102+youkyoungJung@users.noreply.github.com>
2024-02-01 13:00:10 +0900

    feat: [#S1012A402-189] [구매자]주문 내역 상세 조회

--- backend/src/main/java/org/ssafy/pasila/domain/order/repository/OrderRepository.java
+++ backend/src/main/java/org/ssafy/pasila/domain/order/repository/OrderRepository.java
@@ -1,10 +1,20 @@
 package org.ssafy.pasila.domain.order.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.ssafy.pasila.domain.order.dto.OrderDto;
 import org.ssafy.pasila.domain.order.entity.Order;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    public List<Order> findAllByMemberId(Long id);
+    List<Order> findAllByMemberId(Long id);
+
+    @Query("Select new org.ssafy.pasila.domain.order.dto.OrderDto" +
+            "(o.id, o.orderCnt, o.name, o.address, o.price, o.createdAt, o.status," +
+            " o.productOption.id, o.productOption.name, o.productOption.product.name, o.productOption.product.thumbnail) " +
+            "from Order o " +
+            "where o.id = :id")
+    Optional<OrderDto> findOrderDtoById(Long id);
 }
