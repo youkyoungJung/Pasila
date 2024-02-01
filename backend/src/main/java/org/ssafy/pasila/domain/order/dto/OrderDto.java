85cb19c966bf22e2ebde0daaf47155150837a84a
정유경 <76727102+youkyoungJung@users.noreply.github.com>
2024-02-01 10:04:50 +0900

    feat: [#S10P12A402-191] [구매자] 주문 내역 조회

--- backend/src/main/java/org/ssafy/pasila/domain/order/dto/OrderDto.java
+++ backend/src/main/java/org/ssafy/pasila/domain/order/dto/OrderDto.java
@@ -1,14 +1,16 @@
 package org.ssafy.pasila.domain.order.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.ssafy.pasila.domain.member.entity.Member;
-import org.ssafy.pasila.domain.product.entity.ProductOption;
+import org.ssafy.pasila.domain.order.entity.Order;
+import org.ssafy.pasila.domain.order.entity.Status;
 import java.time.LocalDateTime;
 
-//1. 주문 생성할 때
 @Getter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderDto {
@@ -20,7 +22,24 @@ public class OrderDto {
     private Integer price;
     private LocalDateTime createdAt;
     private LocalDateTime deletedAt;
+    private Status status;
     private Member member;
-    private ProductOption productOption;
+    private Long productOptionId;
+    private String productOptionName;
+    private String productName;
 
+    public OrderDto(Order order) {
+        this.id = order.getId();
+        this.orderCnt = order.getOrderCnt();
+        this.name = order.getName();
+        this.address = order.getAddress();
+        this.price = order.getPrice();
+        this.createdAt = order.getCreatedAt();
+        this.deletedAt = order.getDeletedAt();
+        this.status = order.getStatus();
+        this.member = order.getMember();
+        this.productOptionId = order.getProductOption().getId();
+        this.productOptionName = order.getProductOption().getName();
+        this.productName = order.getProductOption().getProduct().getName();
+    }
 }
--- backend/src/main/java/org/ssafy/pasila/domain/order/repository/OrderRepository.java
+++ backend/src/main/java/org/ssafy/pasila/domain/order/repository/OrderRepository.java
@@ -3,6 +3,8 @@ package org.ssafy.pasila.domain.order.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.ssafy.pasila.domain.order.entity.Order;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+import java.util.List;
 
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    public List<Order> findAllByMemberId(Long id);
 }
