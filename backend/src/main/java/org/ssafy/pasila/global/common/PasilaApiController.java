48425cea50ececc06726c82c06d2ec4487a74afb
유소연 <jjellyduck@gmail.com>
2024-02-01 23:34:55 +0900

    feat: [#S10P12A402-184] add welcome api

---
+++ backend/src/main/java/org/ssafy/pasila/global/common/PasilaApiController.java
@@ -0,0 +1,29 @@
+package org.ssafy.pasila.global.common;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.ssafy.pasila.domain.apihandler.ApiCommonResponse;
+
+@RestController
+@RequestMapping("/api")
+public class PasilaApiController {
+
+    @Operation(summary = "Welcome", description = "파시라 API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = String.class))})
+    })
+    @GetMapping
+    public ApiCommonResponse<?> helloPasila() {
+
+        return ApiCommonResponse.successResponse(HttpStatus.OK.value(), "Welcome to Pasila API.");
+
+    }
+}
