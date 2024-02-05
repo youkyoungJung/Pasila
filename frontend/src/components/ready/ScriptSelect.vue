0e97fe05b5c89297110c88c78c63f7a0d6ed9bac
parkjiwon <wldnjs11234@naver.com>
2024-02-05 11:06:32 +0900

    fix: [#S10P12A402-210] 대본생성 오류 해결

--- frontend/src/components/ready/ScriptSelect.vue
+++ frontend/src/components/ready/ScriptSelect.vue
@@ -29,11 +29,12 @@ const selectAccent = (i) => {
 const script = ref('')
 const makeScript = () => {
   data.value.description = script.value.replaceAll('\n', '')
+  console.log(data.value)
   getScript(data.value, sendData, failLog)
 }
 
 const sendData = (e) => {
-  emits('script', e)
+  emits('script', e.data)
 }
 
 const failLog = () => {
