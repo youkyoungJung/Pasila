c9f2cf5d547c255c8dfb5456413c76aa5c581972
GyeongHayng Jun <jgh.hyang@gmail.com>
2024-02-05 10:48:34 +0900

    feat: [#S10P12A402-233] 마이페이지 비밀번호 입력여부에 따른 처리

--- backend/src/main/java/org/ssafy/pasila/domain/member/service/MemberService.java
+++ backend/src/main/java/org/ssafy/pasila/domain/member/service/MemberService.java
@@ -25,39 +25,49 @@ public class MemberService {
 
     private final S3Uploader s3Uploader;
 
-    /** 사용자 정보 수정 메서드 */
+    /**
+     * 사용자 정보 수정 메서드
+     */
     @Transactional
     //TODO 비밀번호 암호화, 계좌번호 암호화
     public Long updateMember(Long id, PersonalInfoDto request, MultipartFile newImageFile) throws IOException {
         Member result = getMemberById(id);
-        /* 비밀번호 암호화 */
-//        request.setPassword(암호화 메서드);
         /* 계좌번호 암호화 */
 //        request.setAccount(암호화 메서드);
-        result.updateMember(request);
+        if (request.getPassword().isEmpty()) {
+            result.updateMember(request);
+        } else {
+            /* 비밀번호 암호화 */
+//            request.setPassword(암호화 메서드);
+            result.updateMemberWithPw(request);
+        }
         handleImage(result, newImageFile);
         return result.getId();
     }
 
-    /** 채널정보 수정 메서드 */
+    /**
+     * 채널정보 수정 메서드
+     */
     @Transactional
     public Long updateChannel(Long id, String description) {
         Member member = memberRepository.findById(id)
-                .orElseThrow(()-> new RestApiException(ErrorCode.UNAUTHORIZED_REQUEST));
+                .orElseThrow(() -> new RestApiException(ErrorCode.UNAUTHORIZED_REQUEST));
         member.updateChannel(description);
         return member.getId();
     }
 
-    /** 멤버 조회 메서드 */
-    private Member getMemberById(Long id){
+    /**
+     * 멤버 조회 메서드
+     */
+    private Member getMemberById(Long id) {
         return memberRepository.findById(id)
-                .orElseThrow(()-> new IllegalArgumentException("해당 아이디에 대한 멤버 정보가 없습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("해당 아이디에 대한 멤버 정보가 없습니다."));
     }
 
     /**
      * 이미지가 있을 경우 S3Upload에 접근, upload
      * 반환된 url을 바탕으로 memberProfile에 저장
-     * */
+     */
     private void handleImage(Member member, MultipartFile image) throws IOException {
         if (!image.isEmpty()) {
             log.info("member:{}", member);
@@ -69,7 +79,7 @@ public class MemberService {
     /**
      * 새로운 이미지의 수정이 필요할 경우
      * 이미지를 삭제하고 새로운 이미지를 등록시킴
-     * */
+     */
     private void handleNewImage(Member member, MultipartFile newImageFile) throws IOException {
         String originImageUrl = member.getProfile();
         log.info("originImageUrl: {}", originImageUrl);
@@ -80,10 +90,11 @@ public class MemberService {
     }
 
     // 이미지가 존재할 경우 삭제하는 경우
+
     /**
      * Profile은 하나이므로 만약 사진이 수정될 경우
      * S3에서의 기존 사진이 삭제되어야함.
-     * */
+     */
     private void deleteImageIfExists(String imageUrl) {
         if (imageUrl != null && !imageUrl.isEmpty()) {
             String fileName = extractFileName(imageUrl);
@@ -93,11 +104,12 @@ public class MemberService {
     }
 
     //imageFile 이름을 추출하는 메서드
+
     /**
      * S3에 저장된 이미지 url은 https://amazon.~~~com/이미지파일명
      * 으로 저장되어있기 때문에 .com/ 을 바탕으로
      * substring 함수를 이용하여 url을 가공시켜 이미지파일명을 추출함.
-     * */
+     */
     private String extractFileName(String imageUrl) {
         String splitStr = ".com/";
         return imageUrl.substring(imageUrl.lastIndexOf(splitStr) + splitStr.length());
@@ -105,18 +117,18 @@ public class MemberService {
 
     public boolean checkEmail(String email) {
         Optional<Member> member = memberRepository.findByEmail(email);
-        if(member.isPresent()){
+        if (member.isPresent()) {
             return false; //이미 존재하는 경우
-        }else {
+        } else {
             return true; // 생성가능
         }
     }
 
     public boolean checkChannel(String channel) {
         Optional<Member> member = Optional.ofNullable(memberRepository.findByChannel(channel));
-        if(member.isPresent()){
+        if (member.isPresent()) {
             return false; // 이미 존재하는 경우
-        }else {
+        } else {
             return true;  // 생성가능
         }
     }
@@ -124,7 +136,7 @@ public class MemberService {
     @Transactional
     public void join(Member member, MultipartFile profileFile) throws IOException {
         // profileFile 을 S3에서 받아온 url 주소를 member profile
-        if(!profileFile.isEmpty()) {
+        if (!profileFile.isEmpty()) {
             String url = s3Uploader.upload(member.getId().toString(), profileFile, "member");
             member.addProfile(url);
         }
@@ -133,9 +145,9 @@ public class MemberService {
 
     public Member login(String email, String password) {
         Optional<Member> findMember = memberRepository.findByEmail(email);
-        findMember.orElseThrow(()->new IllegalStateException("The email address does not exit."));
+        findMember.orElseThrow(() -> new IllegalStateException("The email address does not exit."));
 
-        if( findMember.get().getPassword().equals(password)){
+        if (findMember.get().getPassword().equals(password)) {
             throw new IllegalStateException("Invalid email or password.");
         }
         return findMember.get();
