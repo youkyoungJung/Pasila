0768c8ee2a7f6e6c6f9ee146fed599aecbb6c7c3
정유경 <76727102+youkyoungJung@users.noreply.github.com>
2024-02-06 17:22:50 +0900

    feat: [#S10P12A402-227] Swagger 작성

--- backend/src/main/java/org/ssafy/pasila/domain/sell/api/SellController.java
+++ backend/src/main/java/org/ssafy/pasila/domain/sell/api/SellController.java
@@ -1,6 +1,12 @@
 package org.ssafy.pasila.domain.sell.api;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -15,11 +21,17 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/sell")
 @Slf4j
+@Tag(name = "Sell", description = "Sell API")
 public class SellController {
 
     private final SellService sellService;
 
     @Operation(summary = "GetSellProdcut", description = "[판매자] 주문 내역 전체 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공",
+                    content = {
+                            @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = OrderManagementDto.class)))
+                    })})
     @GetMapping
     public ApiCommonResponse<?> getSellProductList(@RequestParam Long id){ //판매자 id
 
@@ -32,6 +44,11 @@ public class SellController {
      * 판매자 주문 내역 상세 조회
      * */
     @Operation(summary = "GetSellDetailList", description = "[판매자] 주문 내역 상세 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공",
+                    content = {
+                            @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = OrderManagementDetailDto.class)))
+                    })})
     @GetMapping("/{id}/detail")
     public ApiCommonResponse<?> getSellProductDetail(@PathVariable String id){ //제품 id
 
