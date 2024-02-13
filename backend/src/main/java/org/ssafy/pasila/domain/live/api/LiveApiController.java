0d6e80e2125415acc82a3988bf886d9fda929fe1
최동헌 <ehdgjs456@naver.com>
2024-02-13 22:08:40 +0900

    fix:[#S10P12A402-295] 핸드폰 인증 수정

--- backend/src/main/java/org/ssafy/pasila/domain/live/api/LiveApiController.java
+++ backend/src/main/java/org/ssafy/pasila/domain/live/api/LiveApiController.java
@@ -19,7 +19,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.ssafy.pasila.domain.apihandler.ApiCommonResponse;
-import org.ssafy.pasila.domain.live.dto.ChatLogDto;
+import org.ssafy.pasila.domain.live.dto.chat.ChatLogDto;
 import org.ssafy.pasila.domain.live.dto.request.CreateLiveRequestDto;
 import org.ssafy.pasila.domain.live.dto.request.CreateQsheetRequestDto;
 import org.ssafy.pasila.domain.live.dto.response.CreateQsheetResponseDto;
