e282113c14dc6a08bb2936f546455f8cc234fcf9
GyeongHayng Jun <jgh.hyang@gmail.com>
2024-02-15 14:57:16 +0900

    feat: [#S10P12A402-278] 방송진행시 필요한 정보 추가 제공

--- backend/src/main/java/org/ssafy/pasila/domain/product/service/ProductService.java
+++ backend/src/main/java/org/ssafy/pasila/domain/product/service/ProductService.java
@@ -221,6 +221,7 @@ public class ProductService {
                 .bank(product.getMember().getBank())
                 .account(product.getMember().getAccount())
                 .script(live.getScript())
+                .title(live.getTitle())
                 .fullVideoUrl(live.getFullVideoUrl())
                 .build();
 
